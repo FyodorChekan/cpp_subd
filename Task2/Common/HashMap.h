b23b31814cdb34e5cfca4bccf1a366a5fc735cbc
Komarichev Goergii <71156640+Id3n71@users.noreply.github.com>
2023-12-08 09:21:08 +0300

    Update HashMap.h

--- Task2/Common/HashMap.h
+++ Task2/Common/HashMap.h
@@ -3,27 +3,67 @@
 #include <iostream>
 
 using namespace std;
-
-template <typename keyType, typename valueType>
-/*
-* @brief Класс HashMap - реализация структуры данных вида хэшмап. Реализация включает в себя конструкторы,
- * в том числе конструктор копирования, а также деструктор, основные операции, аналогичные парадигме CRUD
- * и переопределение оператора копирования (присваивания)
- *@param countBuckets - размер хэшмапа
- *@param buckets - вектор, который хранит в себе всю структуру данных (в том числе сами пары ключ-значение)
+/**
+* @brief Класс HashMap.
+* @tparam keyType тип данных ключа.
+* @tparam valueType тип данных значения.
 */
+template <typename keyType, typename valueType>
 class HashMap {
 public:
+/**
+* @brief Конструктор по умолчанию.
+*/
     HashMap();
+/**
+* @brief Конструктор копирования.
+* @param t объект HashMap.
+*/
     HashMap(const HashMap &t);
+/**
+* @brief Деструктор.
+*/
     ~HashMap();
+/**
+* @brief Метод вставки пары (Ключ, значение).
+* @param key Ключ.
+* @param value Значение.
+*/
     void Insert(const keyType &key, const valueType &value);
+/**
+* @brief Метод обновление значения по ключу.
+* @param key Ключ.
+* @param value Новое значение.
+*/
     void Update(const keyType &key, const valueType &value);
+/**
+* @brief Метод получения значения по ключу.
+* @param key Ключ.
+* @return Значение.
+*/
     valueType GetValue(const keyType &key);
+/**
+* @brief Метод удаления пары (ключ, значения) по ключу.
+* @param key Ключ.
+*/
     void Remove(const keyType &key);
+/**
+* @brief Метод получения размера HashMap.
+* @return размер HashMap.
+*/
     int GetCountBuckets();
+/**
+* @brief Оператор копирования.
+* @param t объект HashMap.
+*/
     HashMap<keyType, valueType>& operator=(const HashMap<keyType, valueType>& t);
 private:
+/**
+* @brief размер HashMap.
+*/
     int countBuckets;
+/**
+* @brief Массив пар (ключ, значение).
+*/
     vector<vector<pair<keyType, valueType>>> buckets;
 };
